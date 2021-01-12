cd40b6efc80392f2a5f1e7f3e719f4f4312d0d17
vishawa <vishawa01@gmail.com>
2021-01-12 21:38:05 +0530

    initial project

---
+++ demo/src/test/java/vmane/test/FirstTest.java
@@ -0,0 +1,13 @@
+package vmane.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test
+	public void Test1(){
+		Assert.assertEquals(true, false);
+	}
+	
+}
